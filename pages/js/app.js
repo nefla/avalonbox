a4f61e6766da5578cf8067bd03e4aaad4a18fd4f
Nefla <neflasan@gmail.com>
2016-09-16 04:16:50 -0300

    Improve docs and build process

--- pages/js/app.js
+++ pages/js/app.js
@@ -1,7 +1,12 @@
 import avalonbox from '../../src/scripts/avalonbox';
+import highlight from 'highlight.js'
+
 
 document.onreadystatechange = function(){
   if(document.readyState === 'complete'){
+
+    highlight.fixMarkup();
+    highlight.initHighlightingOnLoad();
     avalonbox.run('image-gallery-single');
     avalonbox.run('image-gallery-multiple');
     avalonbox.run('image-gallery-many');
