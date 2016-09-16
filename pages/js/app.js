92a03c66ea5c5687cea9e62b2004adf354e1d62f
Nefla <neflasan@gmail.com>
2016-09-16 04:47:03 -0300

    Swap highlight.js for prismjs

--- pages/js/app.js
+++ pages/js/app.js
@@ -1,12 +1,10 @@
 import avalonbox from '../../src/scripts/avalonbox';
-import highlight from 'highlight.js'
+import prism from 'prismjs'
 
 
 document.onreadystatechange = function(){
   if(document.readyState === 'complete'){
 
-    highlight.fixMarkup();
-    highlight.initHighlightingOnLoad();
     avalonbox.run('image-gallery-single');
     avalonbox.run('image-gallery-multiple');
     avalonbox.run('image-gallery-many');
