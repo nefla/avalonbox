185a5bedb18330239dc7a36a8cedc8764afb5617
Nefla <neflasan@gmail.com>
2016-09-18 17:51:16 -0300

    Add browser-sync and pug as devDep

---
+++ bs-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  files: ['pages/css/*.css', 'pages/js/*.js', 'pages/*.html', 'pages/views/*.pug'],
+  server: {
+    baseDir: ['./', 'pages']
+  },
+  startPath: '/pages/demo.html'
+}
