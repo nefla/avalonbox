9358674cfae558e30351ef01beabc40612315dbf
Nefla <neflasan@gmail.com>
2016-09-28 19:15:32 -0300

    Update build

--- bs-config.js
+++ bs-config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  files: ['pages/css/*.css', 'pages/js/*.js', 'pages/views/*.pug'],
+  files: ['pages/css/*.css', 'pages/js/*.js', 'pages/views/*.pug', 'dist/*.css'],
   proxy: 'localhost:3000'
 }
